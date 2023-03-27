053ccdbd80d1fb533b62dccae609a7655e4c833e
daugbjerg <daugbjerglasse@gmail.com>
2023-03-27 18:44:10 +0200

    first version

---
+++ app/src/main/java/com/example/noteappfirebasetest/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.noteappfirebasetest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void insertData(View view) {
+        Map<String, Object> users = new HashMap<>();
+        users.put("firstname", "Lasse");
+        users.put("lastname", "Lassen");
+        users.put("gender", "male");
+        users.put("age", 26);
+
+
+        db.collection("users")
+                .add(users)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println("added");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("error");
+                        System.out.println(e);
+                    }
+                });
+    }
+
+    public void setData(View view) {
+        Map<String, Object> users = new HashMap<>();
+        users.put("firstname", "William");
+        users.put("lastname", "Badilliam");
+        users.put("born", 2023);
+        users.put("humør", "sur");
+
+        db.collection("users")
+                .document("ada")
+                .set(users)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+}
