5f60902a60a29be30447a99d9969fae6f91fa9fc
daugbjerg <daugbjerglasse@gmail.com>
2023-03-27 18:45:10 +0200

    first versionv.1

--- app/src/main/java/com/example/noteappfirebasetest/MainActivity.java
+++ app/src/main/java/com/example/noteappfirebasetest/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
